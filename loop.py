3abcbee7fadd8db1970a5e81db4655b3a41baf73
AmuthaRavichandran <34593199+AmuthaRavichandran@users.noreply.github.com>
2018-08-08 13:47:53 +0530

    Create loop.py

---
+++ loop.py
@@ -0,0 +1,13 @@
+import math
+import os
+import random
+import re
+import sys
+
+
+
+if __name__ == '__main__':
+    N = int(raw_input())
+for i in range(10):
+    print "{} x {} = {}".format(N, i+1, N * (i + 1))
+    
